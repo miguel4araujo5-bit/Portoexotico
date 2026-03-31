87da48b3be0deddeec0490c1f1a7c9ba0bbc41f3
MiguelAraujo <miguel4araujo5@gmail.com>
2026-03-31 20:16:43 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { HelmetProvider } from 'react-helmet-async';
 import App from './App';
+import { CartProvider } from './context/CartContext';
 import './styles/globals.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <HelmetProvider>
       <BrowserRouter>
-        <App />
+        <CartProvider>
+          <App />
+        </CartProvider>
       </BrowserRouter>
     </HelmetProvider>
   </React.StrictMode>

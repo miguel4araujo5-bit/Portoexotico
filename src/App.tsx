fd2db5ba2f5709fa07dfd729b581e114a893024e
MiguelAraujo <miguel4araujo5@gmail.com>
2026-04-01 22:16:19 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { Helmet } from 'react-helmet-async';
+import AgeGate from './components/common/AgeGate';
 import Header from './components/layout/Header';
 import Footer from './components/layout/Footer';
 import Home from './pages/Home';
